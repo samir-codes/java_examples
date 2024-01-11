1423ceb8f31e8d24450f0965a07240def470f8d9
samir-codes <samirdhakal129@gmail.com>
2024-01-11 12:41:13 -0600

    Area of circle

---
+++ java1.java
@@ -0,0 +1,11 @@
+package java_examples;
+import java.util.*; 
+public class java1 {
+    public static void main(String[] args) {
+        //take radius of circle , calculate area and print 
+        Scanner sc = new Scanner (System.in);
+        double r = sc.nextDouble();
+        double Area = Math.PI*(r*r);
+        System.out.println(Area);
+    }
+}
