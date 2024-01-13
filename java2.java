364bcf66b2d9d1fe4de493c6b750ccfa796ce972
samir-codes <samirdhakal129@gmail.com>
2024-01-13 13:00:26 -0600

    Multiplication table

---
+++ java2.java
@@ -0,0 +1,14 @@
+package java_examples;
+import java.util.*;
+public class java2 {
+    public static void main (String[] args) {
+        // to print the table of input given by user
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1; i<11; i++) {
+            System.out.println(n*i);
+        }
+    }
+    }
+    
+
